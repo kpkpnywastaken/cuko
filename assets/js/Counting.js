f1a81e1b0eb354c102ec68c83f05260e882e74f6
kpkpnyistaken <95221729+kpkpnyistaken@users.noreply.github.com>
2022-04-17 03:27:51 +0700

    Update Counting.js

--- assets/js/Counting.js
+++ assets/js/Counting.js
@@ -8,7 +8,7 @@ function startCounter() {
         jQuery('.love_count').each(function () {
             var $this = jQuery(this);
             jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                duration: 2000000,
+                duration: 2000,
                 easing: 'swing',
                 step: function () {
                     $this.text(Math.ceil(this.Counter) + '+');
